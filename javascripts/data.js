20064f25e5ea12a69230368eb0d7643d452dafc6
Zoe Ames <zoe.ames@ingramcontent.com>
2017-10-09 20:57:33 -0500

    promise pyramid of doom

--- javascripts/data.js
+++ javascripts/data.js
@@ -4,8 +4,92 @@ var dom = require('./dom');
 
 var dinosaurs = [];
 
+
+// The old way - Pyramid of DOOM
+// var dinoGetter = function(){
+// 	$.ajax("./db/dinosaurs.json").done(function(data1){
+// 		console.log("data1", data1);
+// 		data1.dinosaurs1.forEach(function(dino){
+// 			dinosaurs.push(dino);
+// 		});
+// 		$.ajax("./db/dinosaurs2.json").done(function(data2){
+// 			console.log("data2", data2);
+// 			data2.dinosaurs2.forEach(function(dino){
+// 				dinosaurs.push(dino);
+// 			});
+// 			$.ajax("./db/dinosaurs3.json").done(function(data3){
+// 				console.log("data3", data3);
+// 				data3.dinosaurs3.forEach(function(dino){
+// 					dinosaurs.push(dino);
+// 				});
+// 				console.log("dinosaurs after foreach", dinosaurs);
+// 			});
+// 		});
+// 	});
+// };
+
+var firstDinosaurJSON = function(){
+	return new Promise(function(resolve, reject){
+		$.ajax('./db/dinosaurs.json').done(function(data1){
+			resolve(data1.dinosaurs1);
+		}).fail(function(error1){
+			reject(error1);
+		});
+	});
+};
+
+var secondDinosaurJSON = function(){
+	return new Promise(function(resolve, reject){
+		$.ajax('./db/dinosaurs2.json').done(function(data2){
+			resolve(data2.dinosaurs2);
+		}).fail(function(error2){
+			reject(error2);
+		});
+	});
+};
+
+var thirdDinosaurJSON = function(){
+	return new Promise(function(resolve, reject){
+		$.ajax('./db/dinosaurs3.json').done(function(data3){
+			resolve(data3.dinosaurs3);
+		}).fail(function(error3){
+			reject(error3);
+		});
+	});
+};
+
+
+// PROMISE WORKS - promise pyramid of DOOM
+var dinoGetter = function(){
+	firstDinosaurJSON().then(function(results){
+		results.forEach(function(dino){
+			dinosaurs.push(dino);
+		});
+		secondDinosaurJSON().then(function(results2){
+			results2.forEach(function(dino){
+				dinosaurs.push(dino);
+			});
+			thirdDinosaurJSON().then(function(results3){
+				results3.forEach(function(dino){
+					dinosaurs.push(dino);
+				});
+				console.log("dinosaurs", dinosaurs);
+			});
+		});
+	}).catch(function(error){
+		console.log("error", error);
+	});
+};
+
+
+
+
+
+
+
+
 var initializer = function(){
-	dom({name: "T-Rex"});	
+	dinoGetter();	
 };
 
 var getDinosaurs = function(){
